75087d09b7ef405f682dd4dabc038331b6ef2f76
Shah RUtul <shahrutul111@gmail.com>
2017-09-07 12:38:21 +0530

    account setup done

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
 	validates_presence_of :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates_presence_of :email, presence: true, length: { maximum: 255 },  format: { with: VALID_EMAIL_REGEX },uniqueness: { case_sensitive: false }
-	validates_presence_of :password, presence: true, length: { minimum: 6 }
+	validates_presence_of :password, presence: true, length: { minimum: 6 }, allow_nil: true
 	has_secure_password
 end
